6bce55ef668963af972ec57d1654eb205d3c1447
dohlee <dohlee.bioinfo@gmail.com>
2020-01-12 11:41:38 +0900

    Implement GenomeSeqEncoder.

--- src/torchbio/seq/core.py
+++ src/torchbio/seq/core.py
@@ -14,12 +14,23 @@ class GenomeSeqEncoder(object):
     def get_seq(self, chrom, start, end):
         return self.genome_dict[chrom][start:end].seq.upper()
 
-    def get_encoded_seq(self, chrom, start, end):
+    def encode_region(self, chrom, start, end):
         seq = self.get_seq(chrom, start, end)
         
         for base in seq:
             if base not in const.SEQ2INT:
                 raise ValueError(f'Invalid base: {base}')
+        
+        return self.encode_seq(seq)
 
-        return torch.eye(len(seq))[[const.SEQ2INT[base] for base in seq]]
+    def encode_seq(self, seq):
+        return torch.eye(len(seq))[[const.SEQ2INT[base] for base in seq]][:, :4]
+    
+    def decode(self, encoded):
+        indices = torch.argmax(encoded, axis=1).numpy()
+        return ''.join([const.INT2SEQ[i] for i in indices])
 
+if __name__ == '__main__':
+    encoder = GenomeSeqEncoder('/data/project/dohoon/reference/hg38/hg38.fa')
+    encoded = encoder.encode_region('chr1', 10000000, 10000100)
+    decoded = encoder.decode(encoded)
